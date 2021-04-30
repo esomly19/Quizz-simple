54f86eccd135f0d3da580e853f8186979de750b2
Thomas <esomly57@gmail.com>
2021-04-30 13:20:07 +0200

    on change beaucoup de chose router etc

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-
+import VueRouter from 'vue-router';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import Classement from './components/Classement.vue'
+import Game from './components/Game.vue'
 import App from './App.vue'
+import StartQuizz from './components/StartQuizz.vue'
 import CircularCountDownTimer from "vue-circular-count-down-timer";
 Vue.use(CircularCountDownTimer);
 Vue.use(BootstrapVue)
-Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: "history",
+
+  routes: [
+    { path: "/", component: StartQuizz },
+    { path: "/Game/", component: Game},
+ { path: "/classement", component: Classement}
+    
+  ]
+})
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+})
